bdaa8a12051ff693b530c990f3d8055654150aad
S Murtazin <grey.wooden.tractor@gmail.com>
2019-02-17 19:17:05 +0300

    Add in_out

---
+++ in_out.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    
+   // int x = 4;
+   // string y = "hello";
+
+   // cout << x << ' ' << y;
+
+   // std::vector<int> d = {1,3};
+   // cout << d;
+
+   // int x, y;
+   // cin >> x >> y;
+   // cout << x + y << endl;
+
+	string name;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+
+	string surname;
+	cin >> name >> surname;
+	cout << "Hello, " << name << surname << endl;
+
+   return 0;
+}
